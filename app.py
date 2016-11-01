3aae56847de8adbfa5f75c321f2756a1ac89f975
lishiqi <lishiqi@le.com>
2016-11-01 18:35:02 +0800

    增加cookie登录方式

--- app.py
+++ app.py
@@ -11,15 +11,50 @@ from api import PCS, CaptchaError, LoginFailed
 web.config.debug = False
 
 urls = (
-    '/login', 'login',
+    '/cookielogin', 'CookieLogin',
+    '/profile', 'Profile'
 )
 
 app = web.application(urls, locals())
-session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'count': 0})
+session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'count': 0, 'login': False})
 render = web.template.render('templates')
 
 
-class login:
+class CookieLogin:
+    def GET(self):
+        return render.login()
+
+    def POST(self):
+        i = web.input(cookie=None)
+        if not i.cookie:
+            raise web.SeeOther('/cookielogin')
+
+        cookie_list = i.cookie.split(';')
+        pcs = PCS('0', '0')
+        pcs.session.cookies.clear()
+        for c in cookie_list:
+            if not c: continue
+            k, v = c.split('=', 1)
+            pcs.session.cookies[k] = v
+
+        pcs.user['BDUSS'] = pcs.session.cookies['BDUSS']
+        session.pcs = dill.dumps(pcs)
+        session.login = True
+
+        raise web.SeeOther('/profile')
+
+
+class Profile:
+    def GET(self):
+        if session.login is not True:
+            raise web.SeeOther('/cookielogin')
+
+        pcs = dill.loads(session.pcs)
+        assert isinstance(pcs, PCS)
+        return pcs.list_files('/').content
+
+
+class UserPassLogin:
     def GET(self):
         i = web.input(username=None, password=None, captcha=None)
         if not i.username or not i.password:
@@ -27,18 +62,22 @@ class login:
 
         exception = None
         captcha_url = None
-        try:
-            if 'pcs' in session:
-                pcs = dill.loads(session['pcs'])
-                print(session['count'])
-            else:
-                pcs = PCS(i.username, i.password.encode('ascii'))
+        if 'pcs' in session:
+            pcs = dill.loads(session['pcs'])
+            first_login = False
+        else:
+            pcs = PCS(i.username, i.password.encode('ascii'))
+            first_login = True
 
-            if i.captcha: pcs.captcha = i.captcha
-            pcs._login()
+        pcs.relogin_info['captcha'] = i.captcha
+
+        return pcs.list_files('/').content
+
+        try:
+            pcs._login(first_login)
         except CaptchaError as e:
             exception = 'Need captcha'
-            captcha_url = pcs.captcha_url
+            captcha_url = pcs.relogin_info['captcha_url']
         except LoginFailed as e:
             exception = str(e)
         finally:
@@ -57,8 +96,10 @@ class login:
         return self.GET()
 
 
-
-
 if __name__ == '__main__':
+    web.runwsgi()
+    web.loadhook()
+    import web.httpserver
+    web.httpserver.WSGIServer()
     app.run()
 
